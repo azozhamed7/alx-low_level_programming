5eccc177ce3a0343bcc39c178b225a76ab3004b0
azozhamed7 <azozhamed7@gmail.com>
2023-08-10 15:56:42 -0700

    ${commit}

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-/*
- * main  - ther is no dis
+/**
+ * main - Entry point
  *
- * Description: useing puts to print
- *
- * return = it will return zero
- *
+ * Return: always 0 ( success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
