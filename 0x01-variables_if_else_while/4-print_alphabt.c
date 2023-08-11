9174c46e2e1595bd4403de84c5d40a7ce15f6fd9
azozhamed7 <azoz@example.com>
2023-08-11 16:07:03 -0700

    &{commit}

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - 5-print_numbers.c
+ *Return: alwys (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 122; n++)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+	}
+	putchar ('\n');
+	return (0);
+}
