ee5e3fa9bc8b50a313721264d402f2bf97ea8d45
azozhamed7 <azoz@example.com>
2023-08-11 13:31:24 -0700

    &{commit}

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - 5-print_numbers.c
+ *Return: alwys (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar ('\n');
+	return (0);
+
+}
