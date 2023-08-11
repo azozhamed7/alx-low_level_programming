488b08433b8310e7f3b3abe1f2d2bba253fa21ce
azozhamed7 <azoz@example.com>
2023-08-11 15:48:35 -0700

    &{commit}

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(n + 48);
-		if (n == 57)
+		if (n == 9)
 		{
 			continue;
-		
+		}
 		putchar(',');
 		putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
